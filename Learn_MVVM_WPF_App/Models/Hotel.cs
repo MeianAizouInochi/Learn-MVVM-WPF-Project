538529f6fd38a5feaad5922a27307b518ea9a7eb
Meian <meianaizouinochi@gmail.com>
2023-03-08 12:58:30 +0530

    Made Models Folder
    Added Required Classes to Models Folder

---
+++ Learn_MVVM_WPF_App/Models/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_MVVM_WPF_App.Models
+{
+    class Hotel
+    {
+        private readonly ReservationBook Reservation_Book;
+        
+        public string Name { get; }
+
+        public Hotel(string Name)
+        {
+            this.Name = Name;
+
+            this.Reservation_Book = new ReservationBook();
+        }
+    }
+}
---
+++ Learn_MVVM_WPF_App/Models/RoomID.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_MVVM_WPF_App.Models
+{
+    internal class RoomID
+    {
+    }
+}
